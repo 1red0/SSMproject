268bfa0919190c4b5bc3281c950c1517eeae4b16
Darus112 <edarius123@gmail.com>
2022-06-01 23:51:37 +0300

    donedone

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -156,7 +156,7 @@ submitBtn.addEventListener('click', () => {
            quiz.innerHTML = `
            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
 
-           <button onclick="location.reload()">Reload</button>
+           <button class="buttonsub"onclick="location.reload()">Reload</button>
            `
        }
     }
